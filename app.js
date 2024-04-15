b3b41617f65118edd7d43c7c7272a9c6204947f3
Sandesh Gautam <sandesh.gautam9999@gmail.com>
2024-04-15 11:49:08 +0545

    getting started with node

---
+++ app.js
@@ -0,0 +1,6 @@
+const names = require("./names");
+const sayHi = require("./utils");
+
+sayHi(names.sandesh);
+sayHi(names.peter);
+sayHi(names.john);
---
+++ names.js
@@ -0,0 +1,5 @@
+const sandesh = "sandesh";
+const peter = "peter";
+const john = "john";
+
+module.exports = { sandesh, peter, john };
